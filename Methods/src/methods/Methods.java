779105ad0a65b73a80ab4e7ff5c33d879382f31d
mdobaid311 <mdobaid311@gmail.com>
2022-10-02 22:18:50 +0530

    first commit

--- Methods/src/methods/Methods.java
+++ Methods/src/methods/Methods.java
@@ -4,7 +4,27 @@ package methods;
 public class Methods {
 
     public static void main(String[] args) {
-        
+        System.out.println(GCD(35,56));   
     }
     
+    public static boolean isPrime(int num){
+        
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static int GCD(int x,int y){
+        while(x!=y){
+            if(x>y){
+                x=x-y;
+            }else{
+                y=y-x;
+            }
+        }
+        return x;
+    }
 }
