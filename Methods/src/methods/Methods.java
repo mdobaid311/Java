572e539908c7bf0feed2bd103b9ef45ed3d8b0a3
mdobaid311 <mdobaid311@gmail.com>
2022-10-03 11:22:46 +0530

    first commit

--- Methods/src/methods/Methods.java
+++ Methods/src/methods/Methods.java
@@ -4,7 +4,7 @@ package methods;
 public class Methods {
 
     public static void main(String[] args) {
-        System.out.println(GCD(35,56));   
+        System.out.println(discount(200,100,50,100));
     }
     
     public static boolean isPrime(int num){
@@ -26,5 +26,39 @@ public class Methods {
             }
         }
         return x;
+    }
+    
+    public static void show(int ...A){
+        for(int x:A){
+            System.out.println(x);
+        }
+    }
+    
+    public static int maxNum(int ...A){
+        int max =Integer.MIN_VALUE;
+        for(int x:A){
+            if(max<x){
+                max=x;
+            }
+        }
+        return max;
+    }
+    
+    public static int sum(int ...A){
+        int sum=0;
+        for(int x:A){
+            sum+=x;
+        }
+        return sum;
+    }
+    
+    public static double discount(int ...A){
+        int sumOfNumbers=sum(A);
+        if(sumOfNumbers<500){
+            return sumOfNumbers*0.85;
+        }else if(sumOfNumbers>=500){
+            return sumOfNumbers*0.7;
+        }
+        return sumOfNumbers;
     }
 }
