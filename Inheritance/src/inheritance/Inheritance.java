9eb95c283ddddb7756ef8166779656fbad222252
mdobaid311 <mdobaid311@gmail.com>
2022-10-03 20:46:27 +0530

    first commit

--- Inheritance/src/inheritance/Inheritance.java
+++ Inheritance/src/inheritance/Inheritance.java
@@ -21,12 +21,70 @@ class Cuboid extends Square{
     }
 }
 
+class Account{
+    public int accno,phno,balance;
+    public String name,address,dob;
+    
+    public Account(int accno,int phno,int balance,String name,String address,String dob){
+        this.accno  = accno;
+        this.phno = phno;
+        this.balance = balance;
+        this.name = name;
+        this.dob=dob;
+        this.address = address;
+    }
+    
+    public int getBalance(){
+        return balance;
+    }
+    
+    public int getAccno(){
+        return accno;
+    }
+   
+}
+
+class SavingsAccount extends Account{
+    private int FDAmount;
+    
+   public SavingsAccount(int accno,int phno,int balance,String name,String address,String dob){
+       super(accno,phno,balance,name,address,dob);
+   }
+    
+    public void deposit(int amount){
+        balance+=amount;
+    }
+    
+    public void withdraw(int amount){
+        balance-=amount;
+    }
+    
+    public void FixedDeposit(int amount){
+        FDAmount = amount;
+        balance-=amount;
+    }
+    
+    public int getFDAmount(){
+        return FDAmount;
+    }
+    
+}
+
+
+
 
 public class Inheritance {
     public static void main(String[] args) {
-            Cuboid c = new Cuboid(5);
-            
-            System.out.println(c.area());
+        SavingsAccount sa = new SavingsAccount(1234, 9515, 10000, "Mohammed obaid", "baba nagar", "14-08-2000");
+        
+        System.out.println(sa.getBalance());
+        sa.deposit(2000);
+        System.out.println(sa.getBalance());
+        sa.withdraw(1200);
+        System.out.println(sa.getBalance());
+        sa.FixedDeposit(5000);
+        System.out.println(sa.getBalance());
+        System.out.println(sa.getFDAmount());
     }
     
 }
