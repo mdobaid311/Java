e5b32edf31627a4f4f9469d6ab2bfa095fdcd3d2
mdobaid311 <mdobaid311@gmail.com>
2022-10-04 17:27:42 +0530

    first commit

--- Inner class/src/inner/pkgclass/InnerClass.java
+++ Inner class/src/inner/pkgclass/InnerClass.java
@@ -1,5 +1,9 @@
 package inner.pkgclass;
 
+abstract class My{
+    abstract public void show();
+}
+
 class OuterClass{
     
     public int x=10;
@@ -17,11 +21,33 @@ class OuterClass{
     }
 }
 
+class Outer1{
+    public void display(){
+        My m = new My(){
+            @Override
+            public void show(){
+                System.out.println("show");
+            }
+        };
+        m.show(); 
+    }
+}
+
+class Outer{
+    int x =10;
+    static int y =20;
+    static class My{
+     static void show(){
+        System.out.println(y);
+        }
+    }
+}
+
 public class InnerClass {
 
     public static void main(String[] args) {
-        OuterClass oc = new OuterClass();
-        oc.displayOuter();
+        Outer.My m= new Outer.My();
+        Outer.My.show();
     }
     
 }
---
+++ static and final/src/pkgstatic/and/pkgfinal/StaticAndFinal.java
@@ -0,0 +1,54 @@
+package pkgstatic.and.pkgfinal;
+
+class Car {
+    int price = 100000;
+    static int carPrice = 200000;
+
+    static void show(){
+        System.out.println(carPrice);
+    }
+    void display(){
+        System.out.println(price+" "+carPrice);
+    }
+
+    static class Honda{
+        static void display(){
+            System.out.println("Honda city");
+        }
+    }
+    
+    void meth(){
+        Honda.display();
+    }
+    
+}
+
+class Test{
+    static{
+        System.out.println("Block 1");
+    }
+
+    static{
+        System.out.println("Block 2");
+    }
+    
+}
+
+
+
+public class StaticAndFinal {
+    final float PI=3.1425f;
+
+    public static void main(String[] args) {
+//        System.out.println("Main");
+//        Test t = new Test();
+//        Car c = new Car();
+//        Car.carPrice = 300000;
+//        Car.show();
+//        c.display();
+//        c.meth();
+        
+    }
+    
+
+}
