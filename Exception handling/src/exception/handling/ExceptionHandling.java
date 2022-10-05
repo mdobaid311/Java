f006822cc37ec9dfe02515b63c1b26929d4072c2
mdobaid311 <mdobaid311@gmail.com>
2022-10-05 19:21:11 +0530

    first commit

--- Exception handling/src/exception/handling/ExceptionHandling.java
+++ Exception handling/src/exception/handling/ExceptionHandling.java
@@ -1,11 +1,17 @@
 package exception.handling;
 
+class MyException extends Exception{
+    public String toString(){
+        return "My new Exception";
+    }
+}
+
 public class ExceptionHandling {
     
     public static void meth1() throws Exception{
     int x =10;
-    int y=0;
-    if(x<=0||y<=0) throw new Exception();
+    int y=01 ;
+    if(x<=0||y<=0) throw new MyException();
     System.out.println(x*y);
     }
     public static void meth2() throws Exception{meth1();}
@@ -17,6 +23,8 @@ public class ExceptionHandling {
             meth3();
         }catch(Exception e){
             System.out.println(e);
+        }finally{
+            System.out.println("The result is returned");
         }
     }
     
