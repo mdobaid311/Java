6b10f8b7fe1caac12096378b6702c14dec522992
mdobaid311 <mdobaid311@gmail.com>
2022-10-05 19:39:08 +0530

    first commit

--- Exception handling/src/exception/handling/ExceptionHandling.java
+++ Exception handling/src/exception/handling/ExceptionHandling.java
@@ -1,5 +1,9 @@
 package exception.handling;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class MyException extends Exception{
     public String toString(){
         return "My new Exception";
@@ -17,14 +21,27 @@ public class ExceptionHandling {
     public static void meth2() throws Exception{meth1();}
     public static void meth3()throws Exception{meth2();}
     
+    public static void divide() throws FileNotFoundException{
+        try{
+            File fi = new File(System.getProperty("user.dir")+"\\text.txt");
+            Scanner sc = new Scanner(fi);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println(a/0);
+        }catch(Exception e){
+                System.out.println(e);
+            }
+        }
 
     public static void main(String[] args) {
         try{
-            meth3();
+            divide();
         }catch(Exception e){
             System.out.println(e);
         }finally{
             System.out.println("The result is returned");
+            System.out.println(System.getProperty("user.dir")+"\\text.txt");
         }
     }
     
