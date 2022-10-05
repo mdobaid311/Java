778706d9291165a5b465827be4c7ea46ffbf2255
mdobaid311 <mdobaid311@gmail.com>
2022-10-05 22:27:22 +0530

    first commit

--- Strings/src/strings/Strings.java
+++ Strings/src/strings/Strings.java
@@ -1,13 +1,19 @@
 
 package strings;
+
+import java.util.Scanner;
+
 public class Strings {
     public static void main(String[] args) {
-        String str1 = "Hello";
-        String str2 = "hello";
-        String str3 = new String("Hello");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         
-        System.out.println(str1.equals(str3));
-        System.out.println(str1.compareTo(str3));
+        for(int i=0;i<=str.length();i++){
+            System.out.println(str.substring(0, i));
+        }
+        for(int i=str.length()-1;i>0;i--){
+            System.out.println(str.substring(0, i));
+        }
     }
     
 }
