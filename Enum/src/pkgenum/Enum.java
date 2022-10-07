1aab28c54289bf8f66678b603a6878905580bfa2
mdobaid311 <mdobaid311@gmail.com>
2022-10-07 18:42:33 +0530

    first commit

--- Enum/src/pkgenum/Enum.java
+++ Enum/src/pkgenum/Enum.java
@@ -3,9 +3,24 @@ import java.io.*;
 import java.util.*;
 
 enum Dept{
-    CS,IT,CIVIL,ECE;
-    private Dept(){
-            System.out.println("hello");
+    CS("john","Block A"),IT("john","Block B"),CIVIL("john","Block A"),ECE("john","Block A");
+   String head;
+   String location;
+   
+    private Dept(String head,String loc){
+            this.head = head;
+            this.location =loc;
+    }
+    public void display(){
+        System.out.println(this.name()+" "+this.ordinal());
+    }
+    
+    public String headName(){
+        return head;
+    }
+    
+    public String location(){
+        return location;
     }
 }
 
@@ -15,7 +30,10 @@ public class Enum {
         Dept d = Dept.CS;
         System.out.println(d.getClass()); 
         Dept list[] = Dept.values();
-        
+        System.out.println(Arrays.toString(list));
+        d.display();
+        System.out.println(d.headName());
+        System.out.println(d.location());
    }
     
 }
