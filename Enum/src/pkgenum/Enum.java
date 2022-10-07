7d12e26a5593ff87c696e5d5d43a378e690efc97
mdobaid311 <mdobaid311@gmail.com>
2022-10-07 18:36:54 +0530

    first commit

--- Enum/src/pkgenum/Enum.java
+++ Enum/src/pkgenum/Enum.java
@@ -1,7 +1,12 @@
 package pkgenum;
+import java.io.*;
+import java.util.*;
 
 enum Dept{
-    CS,IT,CIVIL,ECE
+    CS,IT,CIVIL,ECE;
+    private Dept(){
+            System.out.println("hello");
+    }
 }
 
 public class Enum {
@@ -9,6 +14,8 @@ public class Enum {
     public static void main(String[] args) {
         Dept d = Dept.CS;
         System.out.println(d.getClass()); 
+        Dept list[] = Dept.values();
+        
    }
     
 }
