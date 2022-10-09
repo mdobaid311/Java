7a7f5f6de3a7e7633d4dcc8b697382f7cd4e5985
mdobaid311 <mdobaid311@gmail.com>
2022-10-09 20:21:05 +0530

    first commit

--- JavaIO/src/javaio/JavaIO.java
+++ JavaIO/src/javaio/JavaIO.java
@@ -3,16 +3,23 @@ package javaio;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.SequenceInputStream;
 import java.util.*; 
 
 public class JavaIO {
 
     public static void main(String[] args) {
         try {
-            FileInputStream fis = new FileInputStream("F:/Java/Test.txt");
-            byte[] b = new byte[fis.available()];
-            fis.read(b);
-            System.out.println(new String(b));
+            FileInputStream f1 = new FileInputStream("F:/Java/Test.txt");
+            FileInputStream f2 = new FileInputStream("F:/Java/copy.txt");
+            
+            SequenceInputStream sq = new SequenceInputStream(f1,f2);
+            
+            FileOutputStream fos = new FileOutputStream("F:/Java/Dest.txt");
+           int x;
+           while((x=sq.read())!=-1)
+            fos.write(x);
+            
         } catch (IOException e) {
             System.out.println(e);
         }
