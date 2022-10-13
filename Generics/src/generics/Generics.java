df3d4e7f3285103ef84d23cc49bd877e4c632746
mdobaid311 <mdobaid311@gmail.com>
2022-10-13 20:15:08 +0530

    first commit

--- Generics/src/generics/Generics.java
+++ Generics/src/generics/Generics.java
@@ -1,6 +1,6 @@
 package generics;
 
-class MyArray<T>{
+class MyArray<T extends Number>{
     T[] objs =(T[]) new Object[10];
     int count=0;
     public void append(T obj){
@@ -9,16 +9,24 @@ class MyArray<T>{
     }
     
     public void getObjs(){
-        for(int i=0;i<objs.length;i++){
+        for(int i=0;i<count;i++){
             System.out.println(objs[i]);
         }
     }
 }
 
+class MyClass2 extends MyArray<Integer>{
+    
+}
+
+class MyClass<T extends String>{
+    
+}
+
 public class Generics {
 
     public static void main(String[] args) {
-       MyArray<Integer> m = new MyArray<>();
+       MyClass2 m = new MyClass2();
        m.append(20);
        m.append(10);
        m.append(50);
