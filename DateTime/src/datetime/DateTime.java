cec48a3404124ff9cf60c85df97b65a815962146
mdobaid311 <mdobaid311@gmail.com>
2022-10-16 14:47:24 +0530

    first commit

--- DateTime/src/datetime/DateTime.java
+++ DateTime/src/datetime/DateTime.java
@@ -1,26 +1,49 @@
 package datetime;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class DateTime {
 
     public static void main(String[] args) {
-//        System.out.println(System.currentTimeMillis());
-//        Date d = new Date();
-//        System.out.println(d.getTime());
-//        System.out.println(d.getDate());
-//        System.out.println(d.getDay());
-//        System.out.println(d.getHours());
-//        System.out.println(d.getMonth());
-//        System.out.println(d.getSeconds());
-//        System.out.println(d.getMinutes());
-//        System.out.println(d.getYear()+1900); 
-          GregorianCalendar gc = new GregorianCalendar();
-          System.out.println(gc.isLeapYear(2023));
-          System.out.println(gc.getFirstDayOfWeek());
-          System.out.println(gc.getTimeZone());
-        
+//            System.out.println(System.currentTimeMillis());
+//            Date d = new Date();
+//            System.out.println(d.getTime());
+//            System.out.println(d.getDate());
+//            System.out.println(d.getDay());
+//            System.out.println(d.getHours());
+//            System.out.println(d.getMonth());
+//            System.out.println(d.getSeconds());
+//            System.out.println(d.getMinutes());
+//            System.out.println(d.getYear()+1900); 
+
+//          GregorianCalendar gc = new GregorianCalendar();
+//          System.out.println(gc.isLeapYear(2023));
+//          System.out.println(gc.getFirstDayOfWeek());
+//          System.out.println(gc.getTimeZone());
+          
+//          LocalDate ld = LocalDate.now();
+//          System.out.println(ld);
+//          System.out.println(ld.getDayOfWeek());
+//          System.out.println(ld.getDayOfYear());
+//          System.out.println(ld.getDayOfMonth());
+//          System.out.println(ld.getMonth());
+//          System.out.println(ld.getYear());
+//          System.out.println(ld.getEra());
+          
+//            ZonedDateTime zdt = ZonedDateTime.now();
+//            System.out.println(zdt);
+//            System.out.println(zdt.getDayOfWeek());
+//            System.out.println(zdt.getDayOfYear());
+//            System.out.println(zdt.getDayOfMonth());
+//            System.out.println(zdt.getMonth());
+//            System.out.println(zdt.getYear());
+//            
+            
+          
+          
     }
     
 }
