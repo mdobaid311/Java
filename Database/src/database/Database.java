8b3d3079d539da06e87c3a09ccce3c08376157f6
mdobaid311 <mdobaid311@gmail.com>
2022-10-16 19:32:58 +0530

    first commit

---
+++ Database/src/database/Database.java
@@ -0,0 +1,10 @@
+
+package database;
+
+public class Database {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ School/src/school/School.java
@@ -0,0 +1,18 @@
+package school;
+import java.sql.*;
+
+public class School {
+
+    public static void main(String[] args) throws Exception{
+        Class.forName("org.sqlite.JDBC");
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:C://sqlite/univ.db"); Statement stm = con.createStatement()) {
+            ResultSet rs = stm.executeQuery("select * from students");
+            while(rs.next()){
+                System.out.print(rs.getInt("SID")+" ");
+                System.out.print(rs.getString("name")+" ");
+                System.out.println(rs.getString("course")+" ");
+            }
+        }
+    }
+    
+}
